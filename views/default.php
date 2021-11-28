14fb440b7bb7eb361d89e8aec6e640562e6cfd1a
Andrew-SE <andrijlutchenko@gmail.com>
2021-11-28 21:21:43 +0100

    styles

--- views/default.php
+++ views/default.php
@@ -6,20 +6,18 @@
     <title><?=$title ?></title>
     <meta name="description" content="<?= $description ?>" />
     <link rel="stylesheet" href="../public/style.css">
-    <link href="https://unpkg.com/tailwindcss@^2/dist/tailwind.min.css" rel="stylesheet">
-
-
     <link rel="icon" href="../public/logo.svg">
 
 </head>
 <body>
 
-    <div class="container  bg-opacity-80">
+    <div class="container">
         <?php
             $this->controller->loadView();
         ?>
     </div>
-    <footer class=" ">
+    <footer>
+
         <p>Copyright © 2021 <img src="../public/logo.svg" alt="logo"> AN-MA SOFT. All Rights Reserved.</p>
     </footer>
 </body>
