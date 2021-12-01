281605961a15a44ed452df1ba14c1d87d26ae366
Andrew-SE <andrijlutchenko@gmail.com>
2021-12-01 21:15:38 +0100

    timetavble testing

---
+++ controllers/TimetableController.php
@@ -0,0 +1,43 @@
+<?php
+
+class TimetableController extends Controller
+{
+
+    function process($parameters)
+    {
+        $timetableArray = $_SESSION['timetable'];
+// $arr = json_decode($_SESSION['timetable']);
+// //var_dump($arr);
+        foreach ($timetableArray as $all => $value) {
+            echo $all . " - " .$value ."<br>";
+        }
+
+// echo "<br>";
+        foreach ($timetableArray as $main => $values) {
+            if($main == "Days")
+                foreach($values as $first => $value){
+                    foreach($value as $second => $val){
+                        if(is_array($val)){ //if($second == "Atoms"){
+                            foreach($val as $third => $vals){
+                                foreach($vals as $fourth => $v){
+                                    if(is_array($v)){
+                                        foreach($v as $fifth => $vv){
+                                            echo $main . " - " .$first ." - " .$second." - " .$third." - ".$fourth." - ".$fifth." - ".$vv ."<br>";
+                                        }
+                                    }
+                                    else{
+                                        echo $main . " - " .$first ." - " .$second." - " .$third." - ".$fourth." - ".$v ."<br>";
+                                    }
+                                }
+                                echo "<br>";
+                            }
+                        }
+                        else{
+                            echo $main . " - " .$first ." - " .$second." - " .$val ."<br>";
+                        }
+                    }
+                    echo "<br>";
+                }
+        }
+    }
+}
